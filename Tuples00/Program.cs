4c752fe19b97b3c0ed18632a396d2328378e6a49
Robert Barr <robert@barrcom.net>
2018-02-21 16:35:29 -0500

    Done... it is what it is.

---
+++ Tuples00/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace Tuples00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*************** General Stuff About Tuples: AKA Fun with Tubles :-) :-> :-| ***************/
+            // 00: Tuble literal with nunamed elements
+
+            var Emp1 = (101, "Bob", "Smith", "Tech Writer 3", true);
+
+            WriteLine($"Emp Id: {Emp1.Item1}\nName: {Emp1.Item2 + " " + Emp1.Item3}\nTitle: {Emp1.Item4}\nActive Employee:{Emp1.Item5}");
+            WriteLine(Emp1);
+
+            // 01: Explicit Tuple type
+            (int, string, string, string, bool) Emp2 = (102, "James", "Gordon", "Tech Manager 4", true); // Notice: 'var' is not required with Tuples
+            WriteLine(Emp2);
+            WriteLine($"Emp2's name is, {Emp2.Item2 + " " + Emp2.Item3}");
+
+            // 02: Naming your Tuple elements
+
+            var Emp3 = (Id: 105, FirstName: "Joel", LastName: "Londagard", JobTitle: "Business Systems Consultant", ActiveEmpoyee: true);
+            WriteLine($"{Emp3.FirstName + " " + Emp3.LastName} is a {Emp3.Item4}"); // Notice "Item4" <T> is not listed in the intellisense but is still available.
+
+            // 03: Arrays
+
+            (string name, int stars)[] Drummers = { ("Vinnie", 5), ("Phil Collins", 3), ("Stuart Copeland", 5), ("Trey Cool", 1) };
+            var sortedDrummers = Drummers.OrderBy(d => d.stars);
+            var v = sortedDrummers.Where(d => d.name.Contains("Vin")).FirstOrDefault();
+            WriteLine(v);
+
+            // 04: A Few more things before we continue on ...
+
+            // A: Tuples are value types with mutable elements
+
+            var Pet1 = ("Max", "dog");
+            var Pet2 = Pet1;
+            Pet2.Item1 = "Meadows";
+            Pet2.Item2 = "cat";
+
+            WriteLine(Pet1);
+            WriteLine(Pet2);
+
+            // B: Tuple types are compaitble by element types ... *NOT* element names
+
+            (string make, string model, int year) c1 = ("Ford", "Tarus", 2012);
+            (string manufactorer, string product, int qty) c2 = ("Betty Crocker", "Cake Mix", 8);
+
+            c2 = c1; // no compiler error! 
+
+            WriteLine(c2); // I was expecting cake!
+
+
+            ReadLine();
+        }
+    }
+}
---
+++ Tuples01/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace Tuples01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 00: Tuples as return type
+            var b = MakeDude();
+            WriteLine(b);
+
+            // 01: More with methods
+
+            int[] scores = { 17, 46, 39, 62, 81, 79, 52, 24 };
+            GradingUtils utils = new GradingUtils();
+            //var s = ch1.GetAvgAndCount(scores);
+            //var (average, count) = ch1.GetAvgAndCount(scores);
+            var (average, count, threshold) = utils.GetAvgAndCount(scores, 51);
+            //WriteLine($"Avg score as {s.avg } and number of students is {s.count}.");
+
+            //WriteLine($"Avg score as {average} and number of students is {count}.");
+
+            WriteLine($"Avg score as {average} and number of students is {count}. Threshold is 51 and the class is { (threshold ? "below" : "above") } average.");
+
+
+
+
+            ReadLine();
+        }
+
+        static (string name, int age) MakeDude() => ("Bob", 37);
+    }
+
+    public class GradingUtils
+    {
+        public (int avg, int count) GetAvgAndCount(int[] scores)
+        {
+            var returnTuple = (avg: 0, count: 0);
+            returnTuple = (returnTuple.avg = scores.Sum() / scores.Count(), returnTuple.count = scores.Count());
+            return returnTuple;
+        }
+
+        public (int avg, int cnt, bool threshold) GetAvgAndCount(int[] scores, int threshold)
+        {
+
+            var info = (avg: 0, count: 0, subAve: true);
+            info = (scores.Sum() / scores.Count(), scores.Count(), info.avg.IsBelowAvg(threshold));
+            return info;
+        }
+    }
+
+    public static class ExtensionMethodsRB
+    {
+        public static bool IsBelowAvg(this int classAvg, int threshold)
+        {
+            if (classAvg < threshold) return true;
+            else return false;
+        }
+    }
+
+}
